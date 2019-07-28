fc2b475167f835df759cb52dbcb2eb76ad3d4fd3
VLasic2001 <vedro.lasic@gmail.com>
2019-07-29 01:15:36 +0200

    Created sport repository and controller

---
+++ FaliJedan/FaliJedan.Domain/Repositories/Interfaces/ISportRepository.cs
@@ -0,0 +1,14 @@
+﻿using FaliJedan.Data.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaliJedan.Domain.Repositories.Interfaces
+{
+    public interface ISportRepository
+    {
+        List<Sport> GetAllSports();
+    }
+}

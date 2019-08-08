d6f1914b11aeb8d4663e0a282326c79d27b92431
AAmanzi <alnick.amanzi@gmail.com>
2019-08-09 00:19:12 +0200

    Added sport item style change on checked

--- FaliJedan/FaliJedan.Web/ClientApp/src/components/Filter/FilterSport.js
+++ FaliJedan/FaliJedan.Web/ClientApp/src/components/Filter/FilterSport.js
@@ -8,21 +8,24 @@ const FilterSport = props => {
     <section className="filter__sport">
       <ul className="filter__sport__list">
         {allSports.map((sport, index) => (
-          <li key={index}>
-            <label>
-              <input
-                type="checkbox"
-                name="sport"
-                checked={props.selectedSports.includes(sport)}
-                onChange={() => props.handleApply(sport)}
-              />
-              <SportIcon
-                className="icon--sport icon--sport-checkbox"
-                sport={sport.name}
-              />
-            </label>
-            <span className="c-az tt-uc fw-b">{sport.name}</span>
-          </li>
+          <React.Fragment key={index}>
+            <input
+              type="checkbox"
+              name="sport"
+              id={`sport_${index}`}
+              checked={props.selectedSports.includes(sport)}
+              onChange={() => props.handleApply(sport)}
+            />
+            <li>
+              <label htmlFor={`sport_${index}`}>
+                <SportIcon
+                  className="icon--sport icon--sport-checkbox"
+                  sport={sport.name}
+                />
+              </label>
+              <span className="c-az tt-uc fw-b">{sport.name}</span>
+            </li>
+          </React.Fragment>
         ))}
       </ul>
     </section>
